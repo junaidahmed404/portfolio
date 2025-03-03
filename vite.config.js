9c8dd4727c47c82b09cf9e8c883b553f0c8d8904
Junaid Ahmed <bhattijunaidahmed404@gmail.com>
2025-03-03 19:36:47 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change this to your GitHub repo name
+  base: '/', // Change this to your GitHub repo name
 });
